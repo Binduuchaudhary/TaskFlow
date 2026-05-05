dd04e72e7ba822d65cdb7ddbcfe965e11589fd74
Binduchoudhary1234 <bindu.comfygen@gmail.com>
2026-05-05 20:57:36 +0530

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,12 +17,10 @@ app.use(helmet());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "*",
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: "https://teamtaskprogress.up.railway.app",
+  credentials: true
+}));
 
 if (process.env.NODE_ENV !== "test") {
   app.use(morgan("dev"));
