3ff1603074d44df64397f261d9b1d7b23adbfa8a
Binduchoudhary1234 <bindu.comfygen@gmail.com>
2026-08-08 19:30:56 +0530

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "https://teamtaskprogress.up.railway.app",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
